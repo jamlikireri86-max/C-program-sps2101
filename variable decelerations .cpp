311b9a386ff4b748e2cde75cbc9c0f5e6951174b
jamlikireri86-max <jamlikireri86@gmail.com>
2025-09-25 15:37:40 +0300

    Add files via upload

---
+++ variable decelerations .cpp
@@ -0,0 +1,15 @@
+#include<stdio.h> 
+//scanf(),printf()
+
+int main() {
+    //variables declaration 
+    
+    int num1=10;
+    int num2=20;
+    int sum;
+    sum = num1 + num2;
+    printf("the sum is %d" , sum);
+    
+    return 0;
+    }
+
